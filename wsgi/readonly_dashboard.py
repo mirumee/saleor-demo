aec1a6eac9c4d3fa352686805956aa0d82e49135
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2017-02-09 11:40:11 +0100

    Add new blocking rule

--- wsgi/readonly_dashboard.py
+++ wsgi/readonly_dashboard.py
@@ -1,11 +1,11 @@
 
 def readonly_dashboard(application):
     def wrapper(environ, start_response):
-        dashboard_url = '/dashboard'
-        if all((environ.get('PATH_INFO').startswith(dashboard_url),
-               environ['REQUEST_METHOD'] == 'POST')):
+        blocked_urls = ['/dashboard', '/profile']
+        is_blocked = any(
+            [environ.get('PATH_INFO').startswith(url) for url in blocked_urls])
+        if is_blocked and environ['REQUEST_METHOD'] == 'POST':
             start_response('204 No Content', [])
             return []
         return application(environ, start_response)
     return wrapper
-
